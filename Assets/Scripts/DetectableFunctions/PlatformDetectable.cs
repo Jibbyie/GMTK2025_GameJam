1f80ca78f6432ceb395ccc5f7808a180727db1ef
jibbyie <jibbyie@gmail.com>
2025-07-31 00:54:36 +0100

    Added moving platform functionality when encircled by a closed loop

---
+++ Assets/Scripts/DetectableFunctions/PlatformDetectable.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class PlatformDetectable : DetectableObject
+{
+    [SerializeField] private float platformSpeed = 5f;
+    private Rigidbody2D platformRB;
+    private bool platformMoved;
+    private bool isMoving;
+    private Transform currentTarget;
+
+    [Header("Points")]
+    [SerializeField] private Transform topPoint;
+    [SerializeField] private Transform bottomPoint;
+
+    private void Awake()
+    {
+        platformRB = GetComponent<Rigidbody2D>();
+    }
+
+    public override void OnDetected()
+    {
+        if (platformRB != null)
+        {
+
+            if(!platformMoved)
+            {
+                currentTarget = topPoint;
+                platformMoved = true;
+            }
+            else
+            {
+                currentTarget = bottomPoint;
+                platformMoved = false;
+            }
+
+            isMoving = true;
+            Debug.Log("Platform moving to: " + currentTarget);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if(isMoving && currentTarget  != null)
+        {
+            MovePlatform();
+        }
+    }
+
+    private void MovePlatform()
+    {
+        // Move towards the target
+        Vector2 newPosition = Vector2.MoveTowards(transform.position, currentTarget.position, platformSpeed * Time.fixedDeltaTime);
+        transform.position = newPosition;
+
+        // Check if we've reached the target
+        if(Vector2.Distance(transform.position, currentTarget.position) < 0.1f)
+        {
+            isMoving = false;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+}
