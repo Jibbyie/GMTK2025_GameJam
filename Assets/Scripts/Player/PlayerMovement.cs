1a7d0bd005fe0a9b8b9a6276eeaf1895340cb390
greensully03 <greensullyfaisal@gmail.com>
2025-07-31 00:08:12 -0700

    New prototyping scene with jump functionality added to PlayerMovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,16 +3,29 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float JumpSpeed = 5f;
     private Rigidbody2D playerRB;
     private float horizontalInput;
+    private GroundDetector GD;
+
+    //Coyote time and jump buffering variables
+    [SerializeField] private float CoyoteTime = 0.1f;
+    public float CoyoteTimer;
+    [SerializeField] private float JumpBuffer = 0.1f;
+    public float JumpBufferTimer;
+
 
     private void Awake()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        GD = GetComponent<GroundDetector>();
+
+        
     }
-    private void FixedUpdate()
+    private void Update()
     {
         Move();
+        Jump();
     }
 
     public void SetWalkSpeed(float newSpeed)
@@ -25,5 +38,34 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
 
         playerRB.linearVelocity = new Vector2(horizontalInput * walkSpeed, playerRB.linearVelocity.y);
+    }
+
+    public void Jump()
+    {
+        if (GD.IsGrounded())
+        {
+            CoyoteTimer = CoyoteTime;
+        }
+        else
+        {
+            CoyoteTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            JumpBufferTimer = JumpBuffer;
+        }
+        else
+        {
+            JumpBufferTimer -= Time.deltaTime;
+        }
+
+
+        if (JumpBufferTimer > 0f && CoyoteTimer > 0f)
+        {
+            playerRB.linearVelocity = new Vector2(playerRB.linearVelocity.x, JumpSpeed);
+            CoyoteTimer = 0f;
+            JumpBufferTimer = 0f;
+        }
     }
 }
