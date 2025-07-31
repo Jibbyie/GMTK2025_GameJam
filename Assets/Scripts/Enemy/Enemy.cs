0fc7949e23d5158effb25e1a6eac93600327744f
jibbyie <jibbyie@gmail.com>
2025-07-31 21:09:06 +0100

    Added player logic - player hp and damage dealing

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,11 +7,13 @@ public class Enemy : MonoBehaviour
     [SerializeField] private bool isRetreating;
     [SerializeField] private float retreatTimer;
     [SerializeField] private float retreatDuration = 1f;
+    [SerializeField] private float damageToPlayer = 20f;
 
     private Rigidbody2D enemyRB;
     private Vector3 retreatDirection;
 
     private PlayerMovement player;
+    private PlayerLogic playerLogic;
 
     private void Awake()
     {
@@ -20,17 +22,22 @@ public class Enemy : MonoBehaviour
         retreatTimer = retreatDuration;
 
         player = FindFirstObjectByType<PlayerMovement>();
+        playerLogic = FindFirstObjectByType<PlayerLogic>();
     }
 
     private void Update()
     {
         if(isRetreating)
         {
+            // Calculate distance from enemy and player
             var enemyPosition = enemyRB.transform.position;
             var playerPosition = player.GetPlayerPosition();
             var distanceBetween = enemyPosition - playerPosition;
+
+            // Store distance normalized for angles
             retreatDirection = distanceBetween.normalized;
 
+            // Move enemy in this retreated direction
             enemyRB.transform.position += retreatDirection * enemySpeed * Time.deltaTime;
             retreatTimer -= Time.deltaTime;
 
@@ -60,6 +67,7 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Shield"))
         {
             isRetreating = true;
+            playerLogic.TakeDamage(damageToPlayer);
             retreatTimer = retreatDuration;
         }
     }
